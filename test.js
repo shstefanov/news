2aa6f9330060853f1252e14025e75f0b69e29333
otnesen <s.h.stefanov@mail.bg>
2011-11-20 02:14:41 +0200

    file manager pre-pre-pre-alpha

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
 function DataManager() { //(container[], put(a) )
-			this.container = new Array();
+			this.container = new Array(); 
 			
 
 			this.put = function (request,something){
@@ -22,15 +22,13 @@ function Client (hostname) {
 	
 	
 	this.get = function (req) {
-		request = instance.request(
-										'GET', 
-										req, //Here is the GET request (example - "/?id=c134")
-  										{'host': host}
-												);
+		request = instance.request( 'GET', req, {'host':host} ); //req - the request
+  												
+											
 		request.end();	
 		request.on('response', function (response) {
-																response.on('data', function (chunk) 	{	data += chunk; 	});
-																response.on('end', function ()				{ 	DM.put(req, data); console.log(data.length);}); 
+							response.on('data', function (chunk) 	{	data += chunk; 	});
+							response.on('end', function ()				{ 	DM.put(req, data); console.log(data.length);}); 
 																
 																		}
 						);
@@ -43,7 +41,17 @@ function Client (hostname) {
 }
 			
 function FileManager () {
+					this.list = new Array();
+					this.fs = require("fs");
+					this.a = new Array();
+					this.fs.readdir('.', function(err,files)
+							{	
+								this.a = files;
+								console.log(this.a);}
+									);
 					
+					
+				
 			}
 
 function DatabaseManager () {
@@ -58,6 +66,9 @@ var c = new Client("focus-news.net");
 
 var DM = new DataManager();
 
-c.get("/?id=c134");
-console.log(DM.container.length); //can't see container, because it is asynchronous and comes after some time
+//c.get("/?id=c134");
+//console.log(DM.container.length); //can't see container, because it is asynchronous and comes after some time
 
+var FM = new FileManager ();
+setTimeout(function() { console.log(a, 4000);});
+//for (var i = 0 ; i < FM.a.length ; i++){console.log(FM.a);}
