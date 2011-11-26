3d33587172fc2f6877143538b270fa68e5f45c86
unknown <pc@homepc.(none)>
2011-11-27 00:21:47 +0200

    FileManager does something.

--- test.js
+++ test.js
@@ -1,14 +1,5 @@
 
-function DataManager() { //(container[], put(a) )
-			this.container = new Array(); 
-			
 
-			this.put = function (request,something){
-						this.container.push(something);
-						
-						}
-						
-			}
 
 function Client (hostname) {
 
