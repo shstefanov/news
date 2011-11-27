b0317067a5a2b269fdc13097042ca19371dadfe6
Stefan Stefanov <s.hr.stefanov@gmail.com>
2011-11-27 06:55:04 +0200

    FileManager does something

--- test.js
+++ test.js
@@ -1,65 +1,84 @@
+var fs = require('fs');
 
 
+function DataManager() { //(container[], put(a) )
+    this.container = new Array();
+    this.put = function (request, something) {
+        this.container.push(something);
+    }
+}
 
-function Client (hostname) {
+function Client(hostname) {
+    var http = require("http");
+    var host = hostname;
+    var request;
+    var data;
+    var instance = http.createClient(80, host);
+    this.get = function (req) {
+        request = instance.request('GET', req, {
+            'host': host
+        }); //req - the request
+        request.end();
+        request.on('response', function (response) {
+            response.on('data', function (chunk) {
+                data += chunk;
+            });
+            response.on('end', function () {
+                DM.put(req, data);
+                console.log(data.length);
+            });
+        });
+    }
+}
 
-	var http = require("http");
-	var host = hostname;
-	var request;
-	var data;
+var counter = 0;
+function FileManager(dir) {
+	this.home = dir;
+	function dir_map(path) {
 	
-	var instance = http.createClient(80, host);
+		var dirlist =  fs.readdirSync(path);
+		
+		console.log(path);
+		for (var idx in dirlist) {
+			
+			var name = dirlist[idx];
+			p = path + '/' + dirlist[idx];
+			d = dirlist[idx];
+			if (fs.statSync(p).isDirectory()){
+				
+				console.log(' ---->'+p);dirlist[idx] = dir_map(p);
+				dirlist[idx].type = 'directory';
+				dirlist[idx].name = name;
+			}
+			else {
+				console.log(p);
+				dirlist[idx].type = 'file';
+				dirlist[idx].name = name;
+			}
+		}
+		console.log('<----'+path);
+		return dirlist;
+	}
 	
-	
-	
-	this.get = function (req) {
-		request = instance.request( 'GET', req, {'host':host} ); //req - the request
-  												
-											
-		request.end();	
-		request.on('response', function (response) {
-							response.on('data', function (chunk) 	{	data += chunk; 	});
-							response.on('end', function ()				{ 	DM.put(req, data); console.log(data.length);}); 
-																
-																		}
-						);
-								}
-	
-	
-	
+	this.list = dir_map(dir);
+}
 
+function DatabaseManager() {
 
 }
-			
-function FileManager () {
-					this.list = new Array();
-					this.fs = require("fs");
-					this.a = new Array();
-					this.fs.readdir('.', function(err,files)
-							{	
-								this.a = files;
-								console.log(this.a);}
-									);
-					
-					
-				
-			}
 
-function DatabaseManager () {
-			
-			}
+function EventBus() {
 
-function EventBus () {
-			
-			}
-			
+}
+
 var c = new Client("focus-news.net");
 
 var DM = new DataManager();
 
-//c.get("/?id=c134");
-//console.log(DM.container.length); //can't see container, because it is asynchronous and comes after some time
 
-var FM = new FileManager ();
-setTimeout(function() { console.log(a, 4000);});
-//for (var i = 0 ; i < FM.a.length ; i++){console.log(FM.a);}
+
+FM = new FileManager('./store');
+console.log(FM.list);
+
+
+
